e2cfc7942e0e8f5428d3ce25aade28cf0c772748
Egorka <errore746@gmail.com>
2023-05-03 00:00:04 +0500

    pyssword v2

--- password.py
+++ password.py
@@ -6,9 +6,9 @@ pasAlpMini = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
               "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 passNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
-password_length = 15
+passLength = 15
 
 password = ''.join([str(random.choice(passAlp + pasAlpMini + passNum))
-                   for i in range(password_length)])
+                   for i in range(passLength)])
 
 print("Пароль👉: ", password)
