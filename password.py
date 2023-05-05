59dbecb6f9a1f61cf7f7d72f5cdc102180f9fd86
Egorka-256 <egorkakasdfgh0@gmail.com>
2023-05-05 13:12:08 +0500

    v2

--- password.py
+++ password.py
@@ -1,14 +1,16 @@
 import random
 
-passAlp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-           "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "/", "!", "?", "*", "$", "-", "_"]
-pasAlpMini = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-              "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+passSymbols = ["/", "!", "?", "*", "$", "-", "_", "#", "&"]
+
+passAlp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+pasAlpMini = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 passNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
-passLength = 15
+passLength = int(input("Длина вашего пароля: "))
 
-password = ''.join([str(random.choice(passAlp + pasAlpMini + passNum))
+password = ''.join([str(random.choice(passAlp + pasAlpMini + passNum + passSymbols))
                    for i in range(passLength)])
 
 print("Пароль👉: ", password)
